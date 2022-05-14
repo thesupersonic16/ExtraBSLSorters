5e760d351c91c5a0607bd366860f8dc05a4b6fae
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2022-05-15 02:38:29 +1000

    Added Code

---
+++ BetterSongList.PlayCountSort/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BetterSongList;
+using BetterSongList.PlayCountSort.Sorters;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BetterSongList.PlayCountSort
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Info("BetterSongList.PlayCountSort initialized.");
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            SortMethods.RegisterPrimitiveSorter(new PlayCountSorter());
+        }
+    }
+}
---
+++ BetterSongList.PlayCountSort/Sorters/PlayCountSorter.cs
@@ -0,0 +1,61 @@
+﻿using BetterSongList.Interfaces;
+using BetterSongList.SortModels;
+using BetterSongList.Util;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BetterSongList.PlayCountSort.Sorters
+{
+    public class PlayCountSorter : ITransformerPlugin, ISorterPrimitive, ISorterWithLegend
+    {
+        public string name => "Play Count";
+
+        public bool visible => true;
+
+        public bool isReady => _playerDataModel != null;
+
+        private PlayerDataModel _playerDataModel;
+
+        public Task Prepare(CancellationToken cancelToken) => Prepare();
+        public Task Prepare()
+        {
+            var objects = Resources.FindObjectsOfTypeAll<PlayerDataModel>();
+            if (objects != null)
+                _playerDataModel = objects.FirstOrDefault();
+            return Task.CompletedTask;
+        }
+
+        public int GetPlayCount(IPreviewBeatmapLevel level)
+        {
+            if (_playerDataModel == null)
+                Prepare();
+
+            if (level == null)
+                return -1;
+            
+            int count = 0;
+            foreach (var statsData in _playerDataModel.playerData.levelsStatsData.Where(t => t.levelID == level.levelID))
+                count += statsData.playCount;
+            return count;
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> BuildLegend(IPreviewBeatmapLevel[] levels) =>
+            SongListLegendBuilder.BuildFor(levels, (level) =>
+        {
+            return $"{GetPlayCount(level)}";
+        });
+
+        public float? GetValueFor(IPreviewBeatmapLevel level)
+        {
+            return GetPlayCount(level);
+        }
+
+        public void ContextSwitch(SelectLevelCategoryViewController.LevelCategory levelCategory, IAnnotatedBeatmapLevelCollection playlist) { }
+    }
+}
